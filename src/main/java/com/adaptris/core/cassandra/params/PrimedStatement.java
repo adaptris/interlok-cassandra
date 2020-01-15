17d4fb27d9ce4c11e335ffd8f376ceefd8089edb
Sebastien Belin <sebastien.belin@adaptris.com>
2020-01-15 06:34:46 +0000

    Fixed compile errors due to removal of commons lang 2.6 dependency.

--- src/main/java/com/adaptris/core/cassandra/params/PrimedStatement.java
+++ src/main/java/com/adaptris/core/cassandra/params/PrimedStatement.java
@@ -1,6 +1,6 @@
 package com.adaptris.core.cassandra.params;
 
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import com.datastax.driver.core.PreparedStatement;
 
